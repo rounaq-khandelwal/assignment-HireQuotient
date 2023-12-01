ce950483db78f70f2cb0a9dcf0385c699d1088a6
Rounaq Khandelwal <rounaqkhandelwal24@gmail.com>
2023-12-01 23:00:22 +0530

    Add CSS, edit and delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// EmployeeList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Employee from './Employee.js';
+import Employee from './Employee';
+import './EmployeeList.css'; // Import CSS file
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
+  const [selectedEmployees, setSelectedEmployees] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,24 +23,106 @@ const EmployeeList = () => {
     fetchData();
   }, []);
 
+  const handleEmployeeSelect = (employeeId) => {
+    const isSelected = selectedEmployees.includes(employeeId);
+    if (isSelected) {
+      setSelectedEmployees(selectedEmployees.filter((id) => id !== employeeId));
+    } else {
+      setSelectedEmployees([...selectedEmployees, employeeId]);
+    }
+  };
+
+  const handleEmployeeEdit = async (employeeId, updatedData) => {
+    try {
+      const updatedEmployees = employees.map((employee) => {
+        if (employee.id === employeeId) {
+          return { ...employee, ...updatedData };
+        }
+        return employee;
+      });
+
+      setEmployees(updatedEmployees);
+
+      // Update data using axios put or patch request to the API endpoint
+      await axios.put(
+        `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`,
+        updatedData
+      );
+    } catch (error) {
+      console.error('Error updating employee:', error);
+    }
+  };
+
+  const handleEmployeeDelete = async (employeeId) => {
+    try {
+      const updatedEmployeeList = employees.filter(
+        (employee) => employee.id !== employeeId
+      );
+      setEmployees(updatedEmployeeList);
+
+      // Delete employee data using axios delete request to the API endpoint
+      await axios.delete(
+        `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`
+      );
+    } catch (error) {
+      console.error('Error deleting employee:', error);
+    }
+  };
+
+  const handleDeleteSelected = async () => {
+    try {
+      const updatedEmployeeList = employees.filter(
+        (employee) => !selectedEmployees.includes(employee.id)
+      );
+      setEmployees(updatedEmployeeList);
+
+      // Delete selected employees using axios delete requests to the API endpoint
+      await Promise.all(
+        selectedEmployees.map((employeeId) =>
+          axios.delete(
+            `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`
+          )
+        )
+      );
+      setSelectedEmployees([]);
+    } catch (error) {
+      console.error('Error deleting selected employees:', error);
+    }
+  };
+
   return (
-    <div>
-      <table>
+    <div className="employee-list-container">
+      <table className="employee-table">
         <thead>
-          <tr>
+          <tr className="column-titles">
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th>Actions</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {employees.map((employee) => (
-            <Employee key={employee.id} employee={employee} />
+            <Employee
+              key={employee.id}
+              employee={employee}
+              isSelected={selectedEmployees.includes(employee.id)}
+              handleEmployeeSelect={handleEmployeeSelect}
+              handleEmployeeEdit={handleEmployeeEdit}
+              handleEmployeeDelete={handleEmployeeDelete}
+            />
           ))}
         </tbody>
       </table>
+      <div>
+        <button
+          onClick={handleDeleteSelected}
+          disabled={selectedEmployees.length === 0}
+        >
+          Delete Selected
+        </button>
+      </div>
     </div>
   );
 };
--- src/Employee.js
+++ src/Employee.js
@@ -1,26 +1,91 @@
 // Employee.js
-import React from 'react';
+import React, { useState } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Cancel';
 
-const Employee = ({ employee }) => {
-  const { id, name, email, role } = employee;
+const Employee = ({
+  employee,
+  isSelected,
+  handleEmployeeSelect,
+  handleEmployeeEdit,
+  handleEmployeeDelete,
+}) => {
+  const {
+    id: employeeId,
+    name: initialName,
+    email: initialEmail,
+    role: initialRole,
+  } = employee;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+  const [email, setEmail] = useState(initialEmail);
+  const [role, setRole] = useState(initialRole);
 
   const handleEdit = () => {
-    // Implement edit functionality
+    setIsEditing(true);
+    handleEmployeeEdit(employeeId, { name, email, role });
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setName(initialName);
+    setEmail(initialEmail);
+    setRole(initialRole);
+    setIsEditing(false);
   };
 
   const handleDelete = () => {
-    // Implement delete functionality
+    handleEmployeeDelete(employeeId);
   };
 
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{name}</td>
-      <td>{email}</td>
-      <td>{role}</td>
+    <tr className={isSelected ? 'selected' : ''}>
+      <td>{employeeId}</td>
       <td>
-        <button onClick={handleEdit}>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
+        {isEditing ? (
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+        ) : (
+          name
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        ) : (
+          email
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <input value={role} onChange={(e) => setRole(e.target.value)} />
+        ) : (
+          role
+        )}
+      </td>
+      <td>
+        {isEditing ? (
+          <>
+            <SaveIcon
+              onClick={handleSave}
+              style={{ cursor: 'pointer', marginRight: '5px' }}
+            />
+            <CancelIcon onClick={handleCancel} style={{ cursor: 'pointer' }} />
+          </>
+        ) : (
+          <>
+            <EditIcon
+              onClick={handleEdit}
+              style={{ cursor: 'pointer', marginRight: '5px' }}
+            />
+            <DeleteIcon onClick={handleDelete} style={{ cursor: 'pointer' }} />
+          </>
+        )}
       </td>
     </tr>
   );
