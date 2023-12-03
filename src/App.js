d85c0a0f68a52bb1c97dbdf1f01ce8ca318cdb31
Rounaq Khandelwal <rounaqkhandelwal24@gmail.com>
2023-12-03 10:51:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-// EmployeeList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Employee from './Employee';
-import './EmployeeList.css'; // Import CSS file
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SearchIcon from '@mui/icons-material/Search';
+import SaveIcon from '@mui/icons-material/Save';
+import ClearIcon from '@mui/icons-material/Clear';
+import './EmployeeList.css'; // Import a CSS file for styling
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
-  const [selectedEmployees, setSelectedEmployees] = useState([]);
+  const [editableRows, setEditableRows] = useState({});
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [tempSearchTerm, setTempSearchTerm] = useState('');
+  const itemsPerPage = 10;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,112 +23,281 @@ const EmployeeList = () => {
           'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
         );
         setEmployees(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+        initializeEditableRows(response.data);
+      } catch (err) {
+        console.error('Error fetching data:', err);
       }
     };
 
     fetchData();
   }, []);
 
-  const handleEmployeeSelect = (employeeId) => {
-    const isSelected = selectedEmployees.includes(employeeId);
-    if (isSelected) {
-      setSelectedEmployees(selectedEmployees.filter((id) => id !== employeeId));
-    } else {
-      setSelectedEmployees([...selectedEmployees, employeeId]);
-    }
+  const initializeEditableRows = (data) => {
+    const initialEditState = {};
+    data.forEach((employee) => {
+      initialEditState[employee.id] = false;
+    });
+    setEditableRows(initialEditState);
   };
 
-  const handleEmployeeEdit = async (employeeId, updatedData) => {
-    try {
-      const updatedEmployees = employees.map((employee) => {
-        if (employee.id === employeeId) {
-          return { ...employee, ...updatedData };
-        }
-        return employee;
-      });
+  const toggleEdit = (id) => {
+    setEditableRows({
+      ...editableRows,
+      [id]: !editableRows[id],
+    });
+  };
 
+  const handleEdit = (id) => {
+    toggleEdit(id);
+  };
+
+  const handleDelete = (id) => {
+    let updatedRows = [...selectedRows]; // Create a copy of selectedRows
+
+    if (id) {
+      // Individual delete
+      updatedRows = updatedRows.filter((rowId) => rowId !== id);
+      const updatedEmployees = employees.filter(
+        (employee) => employee.id !== id
+      );
       setEmployees(updatedEmployees);
-
-      // Update data using axios put or patch request to the API endpoint
-      await axios.put(
-        `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`,
-        updatedData
+      const updatedEditableRows = { ...editableRows };
+      delete updatedEditableRows[id];
+      setEditableRows(updatedEditableRows);
+    } else {
+      // Bulk delete
+      const updatedEmployees = employees.filter(
+        (employee) => !selectedRows.includes(employee.id)
       );
-    } catch (error) {
-      console.error('Error updating employee:', error);
+      setEmployees(updatedEmployees);
+      setSelectedRows([]);
+      initializeEditableRows(updatedEmployees);
+    }
+
+    setSelectedRows(updatedRows); // Update selectedRows
+  };
+
+  const handleInputChange = (e, id, field) => {
+    const updatedEmployees = employees.map((employee) => {
+      if (employee.id === id) {
+        return {
+          ...employee,
+          [field]: e.target.value,
+        };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  };
+
+  const handleSearch = (e) => {
+    setTempSearchTerm(e.target.value);
+  };
+
+  const handleSearchButtonClick = () => {
+    // Trigger search when the search button is clicked
+    // Optionally, you can add additional logic here if needed
+
+    setSearchTerm(tempSearchTerm);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      // Trigger search when ENTER key is pressed
+      setSearchTerm(tempSearchTerm);
     }
   };
 
-  const handleEmployeeDelete = async (employeeId) => {
-    try {
-      const updatedEmployeeList = employees.filter(
-        (employee) => employee.id !== employeeId
-      );
-      setEmployees(updatedEmployeeList);
+  const clearSearch = () => {
+    setTempSearchTerm('');
+    setSearchTerm('');
+  };
 
-      // Delete employee data using axios delete request to the API endpoint
-      await axios.delete(
-        `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`
-      );
-    } catch (error) {
-      console.error('Error deleting employee:', error);
+  const handleCheckboxChange = (id) => {
+    if (selectedRows.includes(id)) {
+      setSelectedRows(selectedRows.filter((rowId) => rowId !== id));
+    } else {
+      setSelectedRows([...selectedRows, id]);
     }
   };
 
-  const handleDeleteSelected = async () => {
-    try {
-      const updatedEmployeeList = employees.filter(
-        (employee) => !selectedEmployees.includes(employee.id)
-      );
-      setEmployees(updatedEmployeeList);
+  const handleSelectAll = () => {
+    const allDisplayedRows = currentItems.map((item) => item.id);
+    setSelectedRows(
+      selectedRows.length === allDisplayedRows.length ? [] : allDisplayedRows
+    );
+  };
 
-      // Delete selected employees using axios delete requests to the API endpoint
-      await Promise.all(
-        selectedEmployees.map((employeeId) =>
-          axios.delete(
-            `https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members/${employeeId}.json`
-          )
-        )
+  const filteredEmployees = employees.filter((employee) => {
+    const { name, email, role } = employee;
+    return (
+      name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      role.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
+  const totalPages = Math.ceil(filteredEmployees.length / itemsPerPage);
+
+  const handleClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <button
+          key={i}
+          onClick={() => handleClick(i)}
+          className={currentPage === i ? 'active' : ''}
+        >
+          {i}
+        </button>
       );
-      setSelectedEmployees([]);
-    } catch (error) {
-      console.error('Error deleting selected employees:', error);
+    }
+    return pageNumbers;
+  };
+
+  const handleFirstPage = () => {
+    setCurrentPage(1);
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
     }
   };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handleLastPage = () => {
+    setCurrentPage(totalPages);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredEmployees.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   return (
-    <div className="employee-list-container">
-      <table className="employee-table">
+    <div className="container">
+      <h1>Employee List</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={tempSearchTerm}
+          onChange={handleSearch}
+          onKeyPress={handleKeyPress}
+          className="search-input"
+        />
+        {tempSearchTerm && (
+          <ClearIcon className="clear-icon" onClick={clearSearch} />
+        )}
+        <button onClick={handleSearchButtonClick}>
+          <SearchIcon className="search-icon" />
+        </button>
+      </div>
+      <table>
         <thead>
-          <tr className="column-titles">
+          <tr>
+            <th>
+              <input
+                type="checkbox"
+                onChange={handleSelectAll}
+                checked={selectedRows.length === currentItems.length}
+              />
+            </th>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th>Action</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {employees.map((employee) => (
-            <Employee
+          {currentItems.map((employee) => (
+            <tr
               key={employee.id}
-              employee={employee}
-              isSelected={selectedEmployees.includes(employee.id)}
-              handleEmployeeSelect={handleEmployeeSelect}
-              handleEmployeeEdit={handleEmployeeEdit}
-              handleEmployeeDelete={handleEmployeeDelete}
-            />
+              className={
+                selectedRows.includes(employee.id) ? 'selected-row' : ''
+              }
+            >
+              <td>
+                <input
+                  type="checkbox"
+                  onChange={() => handleCheckboxChange(employee.id)}
+                  checked={selectedRows.includes(employee.id)}
+                />
+              </td>
+              <td>{employee.id}</td>
+              <td>
+                {editableRows[employee.id] ? (
+                  <input
+                    type="text"
+                    value={employee.name}
+                    onChange={(e) => handleInputChange(e, employee.id, 'name')}
+                  />
+                ) : (
+                  employee.name
+                )}
+              </td>
+              <td>
+                {editableRows[employee.id] ? (
+                  <input
+                    type="text"
+                    value={employee.email}
+                    onChange={(e) => handleInputChange(e, employee.id, 'email')}
+                  />
+                ) : (
+                  employee.email
+                )}
+              </td>
+              <td>
+                {editableRows[employee.id] ? (
+                  <input
+                    type="text"
+                    value={employee.role}
+                    onChange={(e) => handleInputChange(e, employee.id, 'role')}
+                  />
+                ) : (
+                  employee.role
+                )}
+              </td>
+              <td>
+                <div className="actions">
+                  {editableRows[employee.id] ? (
+                    <SaveIcon
+                      className="save-icon"
+                      onClick={() => toggleEdit(employee.id)}
+                    />
+                  ) : (
+                    <EditIcon
+                      className="edit-icon"
+                      onClick={() => handleEdit(employee.id)}
+                    />
+                  )}
+                  <DeleteIcon
+                    className="delete-icon"
+                    onClick={() => handleDelete(employee.id)}
+                  />
+                </div>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
-      <div>
-        <button
-          onClick={handleDeleteSelected}
-          disabled={selectedEmployees.length === 0}
-        >
-          Delete Selected
+      <div className="pagination">{renderPagination()}</div>
+      <div className="delete-selected">
+        <button onClick={() => handleDelete()}>
+          <DeleteIcon />
         </button>
       </div>
     </div>
